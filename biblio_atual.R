768bef418471ce4a8f38fe73415fcfd705ddde09
Natally Celestino <126168130+NatallyCelestino@users.noreply.github.com>
2025-05-07 14:45:47 -0300

    Add files via upload

--- biblio_atual.R
+++ biblio_atual.R
@@ -1,67 +1,57 @@
----
-title: "Artificial intelligence and computer vision in the automatic recognition of plant species: a scientometric review"
-author: "Natally Celestino Gama, Luiz Eduardo Soares Oliveira, Samuel de Pádua Chaves e Carvalho, Eduardo da Silva Leal, Joielan Xipaia dos Santos, Thais Santos Souza, Deivison Venicio Souza"
-date: "2025 - 05 - 07"
-output:
-  rmdformats::readthedown 
----
-
-
-```{r message=FALSE, warning=FALSE}
 library(bibliometrix)
 library(summarytools)
 library(tidyverse)
 library(patchwork)
 library(paletteer)
-```
+
 
 # Scientometric Analysis
 ## Loading data
+  
 
-```{r message=FALSE, warning=FALSE, paged.print=FALSE}
 s <- convert2df('scopus.bib', dbsource = "scopus", format = "bibtex")
 w <- convert2df('savedrecs.bib', dbsource = "wos", format = "bibtex")
 w1<- convert2df('savedrecs (1).bib', dbsource = "wos", format = "bibtex")
-```
+
 
 ## Joining data and removing duplicates
-```{r message=FALSE, warning=FALSE}
+
 U <- mergeDbSources(s,w,w1, remove.duplicated = TRUE)
-```
+
 
 
 ## Summarize data
-```{r message=FALSE, warning=FALSE}
+
 R <- biblioAnalysis(U)
 DS <- summary(object = R, k = 10)
 plot(R, k = 10)
-```
+
 
 
 # Revision
 
 ## Loading and analyzing database structure
-```{r message=FALSE, warning=FALSE, paged.print=FALSE}
-(data <- read_delim("dadostriagem2.0.csv", 
-                    delim = ";", escape_double = FALSE, trim_ws = TRUE))
+
+(data <- read_delim("data/dadostriagem2.0.csv", 
+                    delim = ";", escape_double = FALSE, trim_ws = TRUE))(data <- read.csv2("data/dadostriagem2.0.csv"))
 data$accuracy <- as.numeric(data$accuracy)
 data$N <- as.factor(data$N)
 glimpse(data)
-```
+
 
 ## Graphical View
 
 ### Creating a count table of Main botanical structures (Top 7)
-```{r message=FALSE, warning=FALSE}
+
 cont_eb <- data %>%
   group_by(morphological_structure) %>% 
   summarise(cont = n()) %>% 
   mutate(pct = prop.table(cont)) %>% 
   slice_max(cont, n = 7)
-```
+
 
 ### Botanical Structures Chart
-```{r message=FALSE, warning=FALSE}
+
 (A1 <- cont_eb %>% 
     ggplot(aes(label = scales::percent(pct)))+
     geom_bar(aes(x = reorder(morphological_structure, +cont), y = cont), 
@@ -76,20 +66,20 @@ cont_eb <- data %>%
     ggtitle("A")+
     coord_flip()
 )
-```
+
 
 ### Creating life form types count table (Top 7)
-```{r message=FALSE, warning=FALSE}
+
 cont_tv <- data %>%
   group_by(Life_forms) %>% 
   summarise(cont = n()) %>%
   mutate(pct = prop.table(cont)) %>%
   filter(Life_forms != "NI") %>%
   slice_max(cont, n = 7)
-```
+
 
 ### Chart of Types of Life Forms
-```{r message=FALSE, warning=FALSE}
+
 (A2 <- cont_tv %>%
     ggplot(aes(label = scales::percent(pct))) +
     geom_bar(aes(x = reorder(Life_forms, +cont), y = cont),
@@ -104,19 +94,19 @@ cont_tv <- data %>%
     ggtitle("B")+
     coord_flip()
 )
-```
+
 
 ### Creating extracted features count table (Top 5)
-```{r message=FALSE, warning=FALSE}
+
 cont_re <- data %>%
   group_by(extracted_features) %>% 
   summarise(cont = n()) %>% 
   mutate(pct = prop.table(cont)) %>% 
   slice_max(cont, n = 5)
-```
+
 
 ### Extracted Features Chart
-```{r message=FALSE, warning=FALSE}
+
 (A3 <- cont_re %>%
     ggplot(aes(label = scales::percent(pct))) +
     geom_bar(aes(x = reorder(extracted_features, +cont), y = cont), 
@@ -131,20 +121,20 @@ cont_re <- data %>%
     ggtitle("B") +
     coord_flip()
 )
-```
+
 
 ### Creating plant origin count table (Top 5)
-```{r message=FALSE, warning=FALSE}
+
 cont_orig <- data %>%
   filter(country_of_origin != "NI") %>%
   group_by(country_of_origin) %>% 
   summarise(cont = n()) %>% 
   mutate(pct = prop.table(cont)) %>% 
   slice_max(cont, n = 5)
-```
+
 
 ### Chart of countries of origin of plants
-```{r message=FALSE, warning=FALSE}
+
 (A4 <- cont_orig %>% 
     ggplot(aes(label = scales::percent(pct))) +
     geom_bar(aes(x = reorder(country_of_origin, +cont), y = cont), 
@@ -159,14 +149,14 @@ cont_orig <- data %>%
     ggtitle("A")+
     coord_flip()
 )
-```
+
 
 ### Creating descriptors table (Top 5)
-```{r message=FALSE, warning=FALSE}
+
 crono <- data %>% 
   mutate(Class = case_when((year <= 2008) ~ "1986-2008",
-                             (year >= 2009 & year <= 2014) ~ "2009-2014",
-                             (year > 2014) ~ "2015-2023")) %>% 
+                           (year >= 2009 & year <= 2014) ~ "2009-2014",
+                           (year > 2014) ~ "2015-2023")) %>% 
   select(extraction_techniques, Class) %>% 
   group_by(extraction_techniques, Class) %>% 
   summarise(n = n()) %>% 
@@ -175,10 +165,10 @@ crono <- data %>%
 crono <- crono %>%
   group_by(Class) %>% 
   slice_max(n, n=5)
-```
+
 
 ### Descriptor Count Chart
-```{r message=FALSE, warning=FALSE}
+
 (B1 <- ggplot(crono) +
     geom_bar(aes(x = Class, fill = extraction_techniques, y= n), 
              position = "dodge", stat = "identity") +
@@ -189,14 +179,15 @@ crono <- crono %>%
     labs(fill = "Feature Descriptors") +
     scale_fill_viridis_d(option = "D")
 )
-```
+
+
 
 ### Creating Table of Classification Techniques by Period (Top 5)
-```{r message=FALSE, warning=FALSE}
+
 crono_clas <- data %>% 
   mutate(Class = case_when((year <= 2008) ~ "1986-2008",
-                             (year >= 2009 & year <= 2014) ~ "2009-2014",
-                             (year > 2014) ~ "2015-2023")) %>% 
+                           (year >= 2009 & year <= 2014) ~ "2009-2014",
+                           (year > 2014) ~ "2015-2023")) %>% 
   select(classification_techniques, Class, accuracy) %>% 
   group_by(classification_techniques, Class)
 
@@ -210,11 +201,11 @@ crono_clas <- crono_clas %>%
 crono_clas <- crono_clas %>%
   group_by(Class) %>% 
   slice_max(n, n=5)
-```
+
 
 
 ### Classifier Count Chart
-```{r message=FALSE, warning=FALSE}
+
 (B2 <- ggplot(crono_clas) +
     geom_bar(aes(x = Class, fill = classification_techniques, y = n), 
              position = "dodge", stat = "identity") +
@@ -226,10 +217,10 @@ crono_clas <- crono_clas %>%
     scale_fill_paletteer_d("MetBrewer::Renoir") +
     ggtitle("A")
 )
-```
+
 
 ### Generating average accuracy graph per classifier
-```{r message=FALSE, warning=FALSE}
+
 (B3 <- crono_clas %>% 
     ggplot(aes(label = scales::number(mean_ac)))+
     geom_bar(aes(x = Class, fill = classification_techniques, y = mean_ac), 
@@ -248,10 +239,10 @@ crono_clas <- crono_clas %>%
     scale_fill_paletteer_d("MetBrewer::Renoir") +
     ggtitle("C")
 )
-```
+
 
 ### Generating a graph of the evolution of CNNs over the years (2015-2023)
-```{r message=FALSE, warning=FALSE}
+
 cnn <- data %>% 
   select(classification_techniques, year, accuracy) %>% 
   filter(classification_techniques == "Convolutional Neural Networks (CNN)", 
@@ -262,18 +253,17 @@ cnn <- data %>%
 (B4 <- cnn %>% 
     ggplot(aes(x = year, y = cont), stat = "identity") +
     geom_line() +
-    theme_classic(base_size = 6) +
+    theme_bw(base_size = 6) +
     theme(axis.text.x=element_text(face = "bold")) +
     scale_fill_manual(values = c("#2c115f")) +
     xlab("Years") +
     ylab("CNN") +
     ggtitle("B")
 )
-```
 
+(graf1 <-(B2 + inset_element(B4, 
+                             left = 0.01, bottom = 0.4, 
+                             right = 0.5, top = 0.9)))
+(graf <- (graf1 / B3 + plot_layout(guides = "collect")))
 
-
-
-
-
-
+ggsave("results/grafico1.png", dpi = 800, width = 6.0, height = 6.0)
